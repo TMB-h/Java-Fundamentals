81687a9e3e902ae3325808921f8f86bf76dd289d
Tristan <125160842+TMB-h@users.noreply.github.com>
2026-08-05 18:27:02 -0400

    Rename class Barquin_LabAssignment6b to Rock_Paper_Scissors

--- Rock Paper Scissors/Rock Paper Scissors.java
+++ Rock Paper Scissors/Rock Paper Scissors.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;   
 import java.util.Random;
 
-public class Barquin_LabAssignment6b
+public class Rock_Paper_Scissors
 {
    public static Scanner keyboard = new Scanner(System.in);
    
